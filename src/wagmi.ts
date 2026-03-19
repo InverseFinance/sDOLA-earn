9417e4070633d5829daf4caf8fd60f7464f209b6
webmass <webmass@inverse.finance>
2026-03-19 21:04:03 +0400

    connection: add binance adapter & customize

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,20 +1,41 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { connectorsForWallets, getDefaultConfig, WalletList } from '@rainbow-me/rainbowkit';
 import {
   mainnet,
 } from 'wagmi/chains';
-import { fallback, http, injected, unstable_connector } from 'wagmi';
+import { fallback, http, injected, unstable_connector, createConfig } from 'wagmi';
+import binanceWallet from '@binance/w3w-rainbow-connector-v2'
+import { injectedWallet, baseAccount, walletConnectWallet, rabbyWallet, metaMaskWallet } from '@rainbow-me/rainbowkit/wallets'
 
-export const config = getDefaultConfig({
-  appName: 'sDOLA Earn',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'placeholder',
-  chains: [
-    mainnet,
-  ],
+const recommendedWalletList: WalletList = [
+  {
+    groupName: 'Recommended',
+    wallets: [
+      injectedWallet,
+      binanceWallet,
+      baseAccount,
+      rabbyWallet,
+      metaMaskWallet,
+      walletConnectWallet,
+    ],
+  },
+];
+
+const connectors = connectorsForWallets(
+  recommendedWalletList,
+  {
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'placeholder',
+    appName: 'sDOLA Earn',
+  }
+)
+
+export const config = createConfig({
   ssr: true,
-    transports: {
+  connectors,
+  chains: [mainnet],
+  transports: {
     [mainnet.id]: fallback([
       unstable_connector(injected),
       http(),
     ]),
   },
-});
+})
